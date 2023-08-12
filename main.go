afe75b293c7df4dde5112105551b908acbdd8740
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-12 12:00:39 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ func main() {
 func divide(x, y float32) (float 32, error) {
   var result float32
 
+  if y == 0 {
+    return result, errors.New("cannot divide by 0")
+  }
+
   result = x / y
   return result
 }
