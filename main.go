ef86b12ecb11471c4e49e9beb3b6f599967f8049
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-12 12:02:33 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 func main() {
 
+  result, err := divide(100.0, 10.0)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  log.Println("result of division is", result)
 }
 
 func divide(x, y float32) (float 32, error) {
