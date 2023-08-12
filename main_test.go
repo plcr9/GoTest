9a5e1cb332cd485de016f25f12566f7b0d8a7b5c
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-12 12:12:25 +0100

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,8 +1,17 @@
 package main
 
+import "testing"
+
 func TestDivide(t *testing.T) {
-  _, err := divide(10.0, 1.0)
-  if err != nil {
-    t.Error("Got an error when we should not have")
-  }
+	_, err := divide(10.0, 1.0)
+	if err != nil {
+		t.Error("Got an error when we should not have")
+	}
+}
+
+func TestBadDivide(t *testing.T) {
+	_, err := divide(10.0, 0)
+	if err == nil {
+		t.Error("Did not get an error when we should have")
+	}
 }
